2206e245453e2b552ac4c696676ac70e95203fbb
Farid <100301265+Farid-03@users.noreply.github.com>
2022-03-23 18:06:23 +0400

    Office

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.slider__inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next">2</button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev">1</button>',
+        infinite: false
+    });
+
+    $('select').styler();
+
+
+
+});
